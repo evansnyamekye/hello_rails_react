7e74ce99574c2e50f7609fdb4c272aae4fbcfa0b
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-02-20 15:59:55 +0000

    Add hello rails react project  files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+greetings = [
+    "Hello, World!",
+    "This is Nyamekye Evans",
+    "Welcome to my website",
+    "I am a software engineer",
+    "I am a full-stack developer",
+    "I am a web developer",
+]
+
+greetings.each do |greeting|
+    Message.create(content: greeting)
+end
